97dcee74964448457fb867fa0372e7df1437e681
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2024-06-27 23:44:37 -0300

    Create teste.php

---
+++ teste.php
@@ -0,0 +1,64 @@
+<?php
+
+    /*
+        $string = 'teste';
+        $teste = 'seria isso?';
+
+        echo "isso é um".$$string;
+        
+        if($nome == 'teste'){
+            echo 'é isso mesmo';
+        }else{
+            echo 'não é isso';
+        }
+  
+        $idade = '18';
+        if($idade == 18){
+            echo 'é isso mesmo';
+        }else{
+            echo 'não é isso';
+        }
+
+        for($i = 0; $i < 10; $i++){
+            echo $i;
+            echo '<hr>';
+        }
+       
+        $i = 0;
+        while($i < 10){
+            echo $i;
+            echo '<hr>';
+            $i++;
+        }
+        
+        printNumber(10);
+        echo '<br>';	
+        pribtNumber(20);
+        function printNumber($number){
+            echo $number;
+        }
+       
+    class Pessoa{
+        public $nome;
+        public $idade;
+        
+        public function __construct($nome, $idade){
+            $this->nome = $nome;
+            $this->idade = $idade;
+        }
+
+        public function printNameEAge(){
+            echo $this->nome.' tem '.$this->idade.' anos';
+        }
+
+    }
+
+    $pessoa = new Pessoa('João', 18);
+    $pessoa->printNameEAge();
+    $pessoa2 = new Pessoa('Maria', 20);
+    $pessoa2->printNameEAge();
+    */
+
+    $arr = [1, 2, 3, 4, 5];
+    echo $arr[0];
+?>
